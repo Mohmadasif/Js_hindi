b09795c9e1fc733a359de279d924fca3e8cb3ff0
Mohmad Asif Lone <asiflone651@gmail.com>
2025-11-10 09:49:54 +0000

    datatype

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let acccountEmail = 'example@gmail.com'
+var accaountPassword = '12345';
+accountCity = 'kashmir'
+let accountState;
+
+// accountId = 2 // not allowed
+acccountEmail = 'hc@gmail.com'
+accaountPassword = '253435'
+accountCity = 'Jammu'
+
+// console.log(accountId);
+console.table([accountId, acccountEmail, accaountPassword, accountCity, accountState])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,4 @@
-console.log("asif")
+console.log("asif") 
+
+
+
